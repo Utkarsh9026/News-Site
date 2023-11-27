cdc919613f0ed4b651006d70087b87859a95841e
Utkarsh9026 <utkarshshahi9935@gamil.com>
2023-11-27 11:21:55 +0530

    update

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function reload(){
 
 async function fetchNews(query){
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    console.log(res)
     const data = await res.json();
     bindData(data.articles);
 }
